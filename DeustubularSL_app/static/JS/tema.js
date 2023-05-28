e569e44388047a3e04f71a1fa6ca62df38b0f9ee
unknown <mikel.colmenero@opendeusto.es>
2023-05-28 18:39:40 +0200

    cambios en el cambio de tema

--- DeustubularSL_app/static/JS/tema.js
+++ DeustubularSL_app/static/JS/tema.js
@@ -3,6 +3,8 @@ function cambiarTema() {
     var celdas = document.getElementsByTagName('td');
     var urls = document.getElementsByTagName('a');
     var header = document.getElementsByTagName('header')
+    var footer = document.getElementsByTagName('footer')
+
     body.classList.toggle('invertir-fondo');
     for (var i = 0; i < celdas.length; i++) {
         celdas[i].classList.toggle('invertir-td');
@@ -10,8 +12,11 @@ function cambiarTema() {
     for (var i = 0; i < urls.length; i++) {
         urls[i].classList.toggle('invertir-urls');
       }
-      for (var i = 0; i < header.length; i++) {
+    for (var i = 0; i < header.length; i++) {
         header[i].classList.toggle('invertir-header');
+      }
+      for (var i = 0; i < footer.length; i++) {
+        footer[i].classList.toggle('invertir-footer');
       }
 
     
